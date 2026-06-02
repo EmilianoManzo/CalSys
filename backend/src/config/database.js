bc5382e24195217dffa905a89ece424e25c38983
EmilianoManzo <emilianomanzomartin@gmail.com>
2026-06-02 16:50:51 -0600

    hotfix server.js

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -15,4 +15,16 @@ const pool = mysql.createPool({
   queueLimit: 0
 });
 
+const pool = mysql.createPool({
+  host:     process.env.DB_HOST,
+  user:     process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port:     process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },  // ← agregar esto
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+
 export default pool;
